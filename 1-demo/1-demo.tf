f42d3077bf2399d4109caee425c53e66d030caab
Ned Bellavance <nbellavance@anexinet.com>
2018-10-05 11:16:21 -0400

    Demo 1 update

--- 1-demo/1-demo.tf
+++ 1-demo/1-demo.tf
@@ -1,1 +1,95 @@
-#Basic AWS VPC
+##################################################################################
+# VARIABLES
+##################################################################################
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "network_address_space" {
+  default = "10.1.0.0/16"
+}
+variable "subnet1_address_space" {
+  default = "10.1.0.0/24"
+}
+variable "subnet2_address_space" {
+  default = "10.1.1.0/24"
+}
+
+##################################################################################
+# PROVIDERS
+##################################################################################
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "us-east-1"
+}
+
+##################################################################################
+# DATA
+##################################################################################
+
+data "aws_availability_zones" "available" {}
+
+##################################################################################
+# RESOURCES
+##################################################################################
+
+# NETWORKING #
+resource "aws_vpc" "vpc" {
+  cidr_block = "${var.network_address_space}"
+  enable_dns_hostnames = "true"
+
+  tags = {
+    "Name" = "1-demo-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+}
+
+resource "aws_subnet" "subnet1" {
+  cidr_block        = "${var.subnet1_address_space}"
+  vpc_id            = "${aws_vpc.vpc.id}"
+  map_public_ip_on_launch = "true"
+  availability_zone = "${data.aws_availability_zones.available.names[0]}"
+
+}
+
+resource "aws_subnet" "subnet2" {
+  cidr_block        = "${var.subnet2_address_space}"
+  vpc_id            = "${aws_vpc.vpc.id}"
+  map_public_ip_on_launch = "true"
+  availability_zone = "${data.aws_availability_zones.available.names[1]}"
+
+}
+
+# ROUTING #
+resource "aws_route_table" "rtb" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.igw.id}"
+  }
+}
+
+resource "aws_route_table_association" "rta-subnet1" {
+  subnet_id      = "${aws_subnet.subnet1.id}"
+  route_table_id = "${aws_route_table.rtb.id}"
+}
+
+resource "aws_route_table_association" "rta-subnet2" {
+  subnet_id      = "${aws_subnet.subnet2.id}"
+  route_table_id = "${aws_route_table.rtb.id}"
+}
+
+##################################################################################
+# OUTPUT
+##################################################################################
+
+output "vpc_id" {
+  value = "${aws_vpc.vpc.id}"
+}
+
